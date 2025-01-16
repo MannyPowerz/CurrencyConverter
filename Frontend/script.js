78826b3f12c14db433b73d8cacda832afe88f01d
Emmanuel Hailu <mannysmart35@gmail.com>
2025-01-16 12:30:40 -0500

    Added basics of foundational functionality of dropdown menu to select currency spcifically and also amount

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,7 +1,21 @@
 async function loadCurrecies() {
-    try {
+    try {   
+        const response = await axios.get('http://localhost:5500/api/currencies');
+        const currencies = response.data; 
 
-    } catch (error) {
+        currencies.forEach(([id, name, symbol]) => {
+            //Extracting from Tuple used in Flask
+            const option = `<option value="${id}">${name} (${id})${symbol ? ' ' + symbol : ''}</option>`;
+            // May change Selcetion to & from currency based on api usecases w/in dropdown menu
+            // baseCurrencySelect to fromSelect
+            // quoteCurrencySelect to toSelect
+            baseCurrencySelect.insertAdjacentHTML('beforeend', option); 
+            quoteCurrencySelect.insertAdjacentHTML('beforeend', option);
+        });
+
+    } 
+    
+    catch (error) {
         console.error('Error loading currencies:', error);
     }
 }
