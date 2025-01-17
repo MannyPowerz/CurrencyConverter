550d6575301de81abad2de7d8d87cb330f3a6cb6
Emmanuel Hailu <mannysmart35@gmail.com>
2025-01-17 13:32:47 -0500

    Added JS code for conversion of currencies from base amount to quote amount.

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,7 +1,12 @@
-async function loadCurrecies() {
-    try {   
+async function loadCurrecies() 
+{
+    try 
+    {   
         const response = await axios.get('http://localhost:5500/api/currencies');
         const currencies = response.data; 
+
+        const baseCurrencySelect = document.getElementById('baseCurrencySelect');
+        const quoteCurrencySelect = document.getElementById('quoteCurrencySelect');
 
         currencies.forEach(([id, name, symbol]) => {
             //Extracting from Tuple used in Flask
@@ -15,11 +20,45 @@ async function loadCurrecies() {
 
     } 
     
-    catch (error) {
+    catch (error) 
+    {
         console.error('Error loading currencies:', error);
     }
 }
 
-async function convertCurrencies(){
+async function convertCurrencies()
+{
+    const baseCurrencySelect = document.getElementById('baseCurrencySelect').value;
+    const quoteCurrencySelect = document.getElementById('quoteCurrencySelect').value;
+    const amount = document.getElementById('amount').value;
+    const resultDiv = document.getElementById('result');
 
+
+    if (!baseCurrencySelect || !quoteCurrencySelect || !amount) 
+    {
+        resultDiv.innerHTML = 'Please fill in all the following fields';
+        return;
+    }
+
+    try 
+    {
+        const response = await axois.get('http://localhost:5500/api/convert', 
+        {
+            params: 
+            {   currency1: baseCurrencySelect,
+                currency2: quoteCurrencySelect,
+                amount: amount
+            }
+        } );
+
+        const result = response.data.converted_amount;
+        resultDiv.innerHTML = '${amount} ${baseCurrencySelect} = ${result} ${quoteCurrencySelect}';
+    }
+
+    catch (error)
+    {
+        console.error('Conversion error: ', error);
+        resultDiv.innerHTML = 'Failed Conversion, please try again.';
+    }
 }
+// add converted converion rate for currency as a function in the website before currencies are converted in terms of amount 
